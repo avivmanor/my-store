390b9c3d1dbc12ced9d5d3fbce93a9143f566dda
avivmanor <68183802+avivmanor@users.noreply.github.com>
2020-07-17 11:30:44 +0300

    gui before i try to erite again

--- Gui.java
+++ Gui.java
@@ -13,85 +13,106 @@ public class Gui {
     public static String selectedEmployeeType = null;
     public static String selectedEmployeeName = null;
     public static boolean loginSuccess = false;
+    public static JComboBox branchBox;
+    public static JTextField nameTf;
+    public static JTextField passTf;
+    public static JButton login;
+    public static JFrame frame;
+    public static String loginAnswer = "not found";
 
     public static void main(String args[]){
 
+        loginPanel();
         while (loginSuccess == false) {
-            loginPanel();
-            if (loginSuccess == true) {
-                System.out.println("in login success =true");
-                actionsPanel(selectedEmployeeType, selectedEmployeeName);
+            Runnable theAction = new Runnable() {
+                @Override
+                public void run() {
+                    loginPressed();
+                }};
+            Thread thread1 = new Thread(theAction);
+            thread1.start();
+            System.out.println("thread started");
+            try {
+                thread1.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            System.out.println("thread ended");
+            //loginPressed();
+       }
+        System.out.println("finished while");
+        if(loginSuccess==true){
+            actionsPanel();
+            System.out.println("in login success =true");
         }
-        System.out.println("after loginPanel");
-        //}
-        //while (loginSuccess == false)
-            if(loginSuccess == true){
-                System.out.println("in login success =true");
-                actionsPanel(selectedEmployeeType, selectedEmployeeName);
-            }
+        System.out.println("first "+loginSuccess);
+   }
 
-    }
-
-    public static void loginPanel(){
+    public static void loginPanel() {
+        System.out.println(" in login panel");
         String[] branchList = {"Holon", "Tel-Aviv", "Raanana", "Jerusalem"};
-        JFrame frame = new JFrame("My First GUI");
+        frame = new JFrame("My First GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,500);
+        frame.setSize(500, 500);
         JLabel mainLabel = new JLabel("Welcome to myGym system!");
         mainLabel.setFont(new Font("Serif", Font.BOLD, 20));
         mainLabel.setBounds(130, 10, 300, 50);
         frame.add(mainLabel);
 
         JLabel branchLabel = new JLabel("pick a branch");
-        JComboBox branchBox = new JComboBox(branchList);
+        branchBox = new JComboBox(branchList);
         branchLabel.setBounds(50, 80, 100, 50);
         branchBox.setBounds(200, 80, 100, 50);
         frame.add(branchLabel);
         frame.add(branchBox);
 
         JLabel nameLabel = new JLabel("Employee name");
-        JTextField nameTf = new JTextField(20);
-        nameLabel.setBounds(50, 180 ,100, 40);
+        nameTf = new JTextField(20);
+        nameLabel.setBounds(50, 180, 100, 40);
         nameTf.setBounds(200, 180, 100, 40);
         frame.add(nameLabel);
         frame.getContentPane().add(nameTf);
 
         JLabel passLabel = new JLabel("Enter password");
-        JTextField passTf = new JTextField(20);
-        passLabel.setBounds(50, 270 ,100, 40);
+        passTf = new JTextField(20);
+        passLabel.setBounds(50, 270, 100, 40);
         passTf.setBounds(200, 270, 100, 40);
         frame.add(passLabel);
         frame.getContentPane().add(passTf);
 
-        JButton login = new JButton("login");
+        login = new JButton("login");
         frame.getContentPane().add(login);
         login.setBounds(200, 360, 100, 50);
         frame.setLayout(null);
         frame.setVisible(true);
+        System.out.println(" finish login panel");
+    }
 
+public static void loginPressed(){
+        System.out.println("in login pressed");
         login.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String selecetedBranch = branchBox.getSelectedItem().toString();
                 String employeeName = nameTf.getText();
                 String employeePass = passTf.getText();
-                String isSuccess = null;
                 try {
-                    isSuccess = loginTrial(selecetedBranch, employeeName, employeePass);
+                    loginAnswer = loginTrial(selecetedBranch, employeeName, employeePass);
                 } catch (IOException ioException) {
                     ioException.printStackTrace();
                 }
-                JOptionPane.showMessageDialog(null, isSuccess);
-                System.out.println(" before false");
-                frame.setVisible(false);
-                System.out.println(" after false");
-
+                if(loginAnswer.equals("found")) {
+                    JOptionPane.showMessageDialog(null, loginAnswer);
+                    frame.setVisible(false);
+                    System.out.println("we are in login pressed,answer id found, "+loginSuccess);
+                }
             }
         });
+        System.out.println("finish login press");
         return;
     }
 
     public static String loginTrial(String branch, String name, String pass) throws UnknownHostException, IOException {
+        System.out.println("in loginTrial");
         Socket connection = new Socket("localhost", 5000);
         OutputStream os = connection.getOutputStream();
         DataOutputStream  dos = new DataOutputStream(os);
@@ -106,10 +127,11 @@ public class Gui {
             loginSuccess = true;
             selectedEmployeeName = name;
         }
+        System.out.println("finish login trial");
         return answer;
     }
 
-    public static void actionsPanel(String selectedEmployeeType, String selectedEmployeeName){
+    public static void actionsPanel(){
         System.out.println(" in actions panel");
         JFrame actionsFrame = new JFrame("Welcome "+selectedEmployeeName+" !");
         JButton signNew,endSub, managerActions, logoff,button4;
@@ -125,9 +147,7 @@ public class Gui {
         actionsFrame.add(button4);
         actionsFrame.setLayout(new GridLayout(2,3));
         actionsFrame.setSize(600,600);
-        System.out.println(" before true");
         actionsFrame.setVisible(true);
-        System.out.println("after true");
         managerActions.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -140,6 +160,8 @@ public class Gui {
                 }
             }
         });
+        System.out.println("finish login panel");
+        return;
     }
 
     public void promoteEmployeePanel(String selectedEmployeeType, String selectedEmployeeName){
